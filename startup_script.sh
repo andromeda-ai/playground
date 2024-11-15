a84cc3d4268e0547e41f60c240c8e3a04b0ce130
Vishvajit Kher <vishvajit79@gmail.com>
2024-11-14 23:17:28 -0500

    Refine SSHD configuration to integrate PAM and add MFA support

--- startup_script.sh
+++ startup_script.sh
@@ -7,12 +7,30 @@ sleep 1
 # Establish the Privilege Separation directory for SSH
 mkdir -p /var/run/sshd
 
-# Refine the SSHD configuration to integrate PAM
-sed -i -e 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' \
-       -e 's/UsePAM no/UsePAM yes/' \
-       -e 's/#PasswordAuthentication yes/PasswordAuthentication yes/' \
-       -e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' \
-       /etc/ssh/sshd_config
+# Refine the SSHD configuration to integrate PAM and remove existing lines if they exist
+sed -i '/ChallengeResponseAuthentication/d' /etc/ssh/sshd_config
+sed -i '/UsePAM/d' /etc/ssh/sshd_config
+sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
+sed -i '/PubkeyAuthentication/d' /etc/ssh/sshd_config
+sed -i '/AuthenticationMethods/d' /etc/ssh/sshd_config
+sed -i '/KbdInteractiveAuthentication/d' /etc/ssh/sshd_config
+
+cat <<EOL >> /etc/ssh/sshd_config
+ChallengeResponseAuthentication yes
+UsePAM yes
+PasswordAuthentication yes
+PermitRootLogin no
+EOL
+
+if [ "$ENABLE_MFA" = "true" ]; then
+    cat <<EOL >> /etc/ssh/sshd_config
+    PasswordAuthentication no
+    PubkeyAuthentication yes
+    AuthenticationMethods publickey,keyboard-interactive
+    KbdInteractiveAuthentication yes
+    EOL
+fi
 
 # Introduce supplementary configuration options to sshd_config
 cat <<EOL >> /etc/ssh/sshd_config
